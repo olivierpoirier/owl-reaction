e3dcf0836e8fe7d972f2bcd1990d71d76c0819e8
olivierpoirier <117477661+olivierpoirier@users.noreply.github.com>
2025-04-07 20:54:19 -0400

    enlève élément de trop

--- owl-reaction/src/App.jsx
+++ owl-reaction/src/App.jsx
@@ -68,7 +68,10 @@ export default function App() {
         <div className="grid grid-cols-1 gap-4">
           {items.map((item) => (
             <div key={item.id} className="border rounded p-2 bg-white shadow">
-              <p className="text-xs text-gray-500 mb-1">Type: {item.type}</p>
+                {/*
+                    <p className="text-xs text-gray-500 mb-1">Type: {item.type}</p>
+                */}
+
 
               {item.type === "IMAGE" && item.image?.url && (
                 <img
@@ -78,15 +81,18 @@ export default function App() {
                 />
               )}
 
+            
               {item.type === "TEXT" && (
                 <div className="text-sm font-medium text-center text-blue-700">
                   {item.text?.plainText || "(Texte vide)"}
                 </div>
               )}
+            
 
-              <p className="text-xs mt-2 text-center text-gray-800 font-semibold">
+              {/*<p className="text-xs mt-2 text-center text-gray-800 font-semibold">
                 {item.name || "Sans nom"}
               </p>
+              */}
             </div>
           ))}
         </div>
